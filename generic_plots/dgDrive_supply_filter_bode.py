387fa2120d0e297f3e04f074bb472553c7335630
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2023-06-23 16:47:45 +0200

    Added PSRR data

--- generic_plots/dgDrive_supply_filter_bode.py
+++ generic_plots/dgDrive_supply_filter_bode.py
@@ -31,7 +31,7 @@ plot = {
     },
     "files": [
         {
-            "filename": "DgDrive PSRR_take2_2023-02-18T01_12_08.csv",
+            "filename": "DgDrive_PSRR_take2_2023-02-18T01_12_08.csv",
             "show": True,
             "parser": "bode100",
             "options": {
